5372075f92274d3d1bc68ae90bc218a5114f3da4
yarangodev <yeisonarango03@gmail.com>
2023-11-18 18:24:53 -0500

    fix: server start

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 /* The code is importing the `AddressInfo` type from the `net` module and the `server` object from the
 `./server/server` file. */
 import { config } from "dotenv";
-import { AddressInfo } from "net";
 import server from "./server/server";
 config();
 
 server.listen(process.env.PORT || 3000, () => {
-  const address = server.address() as AddressInfo;
-  console.log(`Server started on port ${address.port}`);
+  console.log(`Server started`);
 });
